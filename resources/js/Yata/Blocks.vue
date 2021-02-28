a4f0f6cfa463bd71d60f20c930ebb1285aa2aba4
azamat-li <azamatalifullstack@gmail.com>
2021-02-28 20:55:26 +0500

    feat: amend, move Blocks.vue into Yata

---
+++ resources/js/Yata/Blocks.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="top-0  rounded-lg  my-2 mr-3 p-2">
+    <div class="flex flex-wrap ">
+			<Block>
+        v-for="(form, formIndex) of forms"
+        :formProp="form"
+        :key="formIndex"
+        :form="form"
+      >
+			</Block>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex"
+import Block from "@/Jetstream/Block"
+
+export default {
+  components: {
+			Block
+  },
+  computed: {
+    ...mapState(['schoolTimetable']),
+    forms() {
+      return this.schoolTimetable.forms
+    }
+  }
+}
+</script>
+<style lang="css" scoped></style>
