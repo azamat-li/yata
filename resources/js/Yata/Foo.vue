025ad110d587fbccc8d0d8dfe725100aa6b665af
azamat-li <azamatalifullstack@gmail.com>
2021-03-01 19:44:06 +0500

    feat: front, added views

---
+++ resources/js/Yata/Foo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m-2 p-2  ">
+  </div>
+</template>
+
+<script>
+import I18nSwitcher from './I18nSwitcher.vue'
+export default {
+  components: {
+    I18nSwitcher
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ resources/js/Yata/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6">
+                        <div class="flex items-center">
+                            
+                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Do you know, what is it?</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                            Yata is Yet Another Trello App, newbie! </div>
+                        </div>
+                    </div>
+
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                        <div class="flex items-center">
+                            
+                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">How do you use it?</a></div>
+                        </div>
+
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                           Organize your workflow, planner!</div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+</template>
+
+<style lang="scss" scoped></style>
+
