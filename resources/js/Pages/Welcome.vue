b41e86a07ef8dd653bd517c89c37dea0ecb56345
azamat-li <azamatalifullstack@gmail.com>
2021-03-02 15:26:17 +0500

    chore: adding data-cy

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
+            <inertia-link data-cy="dashboard"  v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
                 Dashboard
             </inertia-link>
 
             <template v-else>
-                <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
+                <inertia-link data-cy="login"  :href="route('login')" class="text-sm text-gray-700 underline">
                     Log in
                 </inertia-link>
 
-                <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                <inertia-link data-cy="register" v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
                     Register
                 </inertia-link>
             </template>
